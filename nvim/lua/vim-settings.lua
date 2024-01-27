2a99ed3af3e0d7abb79692311c05e71ced64f8f0
Bartek Kwiecien <5788938+bkwi@users.noreply.github.com>
2024-01-27 17:07:05 +0100

    Easier navigation

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -5,11 +5,13 @@ vim.cmd("set expandtab")
 vim.cmd("set number")
 vim.g.mapleader = " "
 
-
+vim.keymap.set("n", "<C-h>", "<C-w>h", {})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {})
 
 vim.keymap.set("n", "<Right>", "<cmd>:vertical resize +5<cr>", {})
 vim.keymap.set("n", "<Left>", "<cmd>:vertical resize -5<cr>", {})
-
 
 vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float({ border = 'rounded' })<CR>", {})
 
