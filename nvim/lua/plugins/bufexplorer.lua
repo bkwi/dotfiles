ada1446b88d2a7631f15ec0e320e4ab35d19c28d
Bartek Kwiecien <bartoszkwiecien@drata.com>
2024-02-16 12:19:15 +0100

    Removed bufexplorer, added harpoon
    Telescope can be used to view buffers

--- nvim/lua/plugins/bufexplorer.lua
+++
@@ -1,9 +0,0 @@
-
-return {
-    "jlanzarotta/bufexplorer",
-
-    config = function()
-    end
-    -- <Leadev>be opens explorer
-    -- d deletes selected buffer
-}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,28 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	dependencies = { "nvim-lua/plenary.nvim" },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
-	end,
+    config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+
+        -- default mappings:
+        -- <C-n>/<Down>	Next item
+        -- <C-p>/<Up>	Previous item
+        --
+        local telescope = require("telescope")
+
+        telescope.setup({
+            defaults = {
+                mappings = {
+                    n = {
+                        ["<C-d>"] = require("telescope.actions").delete_buffer,
+                    }, -- n
+                },
+            },
+        })
+    end,
 }
